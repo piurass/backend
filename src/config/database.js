5d2e2a9ee254a3f3763602b834f17644588d9001
talvane <talvane.magalhaes@gmail.com>
2020-05-01 16:17:38 -0300

    environment file added

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '120405@!.,',
-    database: 'crypto',
+    host: process.env.HOST,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     define: {
         timestamps: true,
         underscore: true,
