e1f41f3b70812377ec8f6490c6f9ba43604ba291
talvane <talvane.magalhaes@gmail.com>
2020-05-03 20:24:13 -0300

    valid confirmation email

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
+import User from '../models/User';
+
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
@@ -16,6 +18,15 @@ export default async (req, res, next) => {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
         req.userId = decoded.id;
+
+        const { id } = decoded;
+        const user = await User.findByPk(id);
+
+        if (!user.isverified) {
+            return res
+                .status(401)
+                .json({ erro: 'User not confirmed his email yet' });
+        }
 
         return next();
     } catch (error) {
